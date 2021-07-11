e39bc4d88aeb446b9e5ac4efd64f934e5d946522
Fadi <smartfadi161@gmail.com>
2021-07-11 19:19:06 +0500

    Configured React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Button } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
       <p>My Portfolio</p>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
